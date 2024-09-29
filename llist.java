6337cb7fb4c7ba641198e73914c072db43d829db
BaTMaNx2002 <sahilsingh18012002@gmail.com>
2024-09-29 14:11:22 +0530

    Add files via upload

---
+++ llist.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class llist {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(1);
+        System.out.println("Original List: " + list);
+
+        // Create a second linked list and add the original list's elements
+        LinkedList<Integer> list2 = new LinkedList<>(list);
+        
+        // Reverse the second linked list
+        Collections.reverse(list2);
+        System.out.println("Reversed List: " + list2);
+        
+        
+    }
+}
+
