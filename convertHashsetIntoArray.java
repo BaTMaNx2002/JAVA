bada6f558f4b7fa89fd2eba59c93796d3b80acee
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-15 10:33:33 +0530

    Add files via upload

---
+++ convertHashsetIntoArray.java
@@ -0,0 +1,13 @@
+/**
+ * intersectionOfTwoArrays
+ */
+public class convertHashsetIntoArray {
+
+    
+}
+        // Convert HashSet to int[]
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (Integer num : set) {
+            result[index++] = num;
+        }
