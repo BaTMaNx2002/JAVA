3b9387f775ee04369043cacbadd8eeb9dcdadf3f
SAHIL SINGH <sahilsingh18012002@gmail.com>
2024-10-24 16:59:50 +0530

    Add files via upload

---
+++ InheritanceOops.java
@@ -0,0 +1,22 @@
+// Inheritance
+// Inheritance allows a new class to inherit the properties and methods of an existing class. This promotes code reusability.
+
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle is starting");
+    }
+}
+
+class Bike extends Vehicle {
+    void honk() {
+        System.out.println("Bike is honking");
+    }
+}
+
+public class InheritanceOops {
+    public static void main(String[] args) {
+        Bike myBike = new Bike();
+        myBike.start(); // Inherited method
+        myBike.honk();  // Bike's own method
+    }
+}
